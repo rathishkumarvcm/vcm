9a0db8606bd5a8bc02ef5d5e8fa51f3d98365c27
Vijayakumar <vijayakumar.p3@cognizant.com>
2020-01-02 12:45:20 +0530

    Jest button test added

--- __tests__/VerifyMobile-test.js
+++ __tests__/VerifyMobile-test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import VerifyMobile from '../App/Screens/VerifyMobile/VerifyMobileComponent';
 import {shallow} from 'enzyme';
+import { GButtonComponent } from '../App/CommonComponents';
 
 // Snap shot
 it('Match Snapshot', () => {  
@@ -17,6 +18,30 @@ test('function testing',()=>{
   wrapper.isEmpty('text');
   wrapper.setMobileNumber('mobilenumber');
   wrapper.onClickNext();
+});
+
+// Input Component matches the length 
+it('Input text matches with length', () => {
+  const instanceOf = renderer.create(<VerifyMobile />).getInstance();
+  instanceOf.setMobileNumber('12345678900');  
+  expect(instanceOf.state.mobileNumber).toHaveLength(11);
+});
+
+ // Input Component matches with given input
+it('Input text matches with given input',()=>{
+  const instanceOf = renderer.create(<VerifyMobile />).getInstance();
+  instanceOf.setMobileNumber('12345678900');  
+  expect(instanceOf.state.mobileNumber).toEqual('12345678900');
+});
+
+// mocked function call on button click
+it('Called function on button click', () => {       
+  const wrapper = shallow(<VerifyMobile onPress={jest.fn()} />)
+  const spy = jest.spyOn(wrapper.instance(), 'onClickNext');
+  wrapper.instance().forceUpdate();
+  wrapper.find(GButtonComponent).at(2).simulate('press');
+  expect(spy).toHaveBeenCalled();
+  spy.mockClear();  
 });
 
 // // Instead of render method using at each time
--- __tests__/VerifySSN-test.js
+++ __tests__/VerifySSN-test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
 import VerifySSN from '../App/Screens/VerifySSN/VerifySSNComponent';
-// import GButtonComponent from '../App/CommonComponents/GButtonComponent';
-// import { GInputComponent } from '../App/CommonComponents';
+import { GButtonComponent } from '../App/CommonComponents';
 
 describe('Verify SSN ',()=>{
 
@@ -14,7 +13,7 @@ describe('Verify SSN ',()=>{
   });
 
   // Render using shallow - shallow not includes child component
-  test('function testing',()=>{
+  test('render',()=>{
     const component = shallow(<VerifySSN />);  
     component.render();
   });
@@ -28,14 +27,40 @@ describe('Verify SSN ',()=>{
     wrapper.onClickNext();
  });
 
- // Input Component 
-it('Input Text',()=>{
-    const instanceOf = renderer.create(<VerifySSN />).getInstance();
-    instanceOf.setSocialSecurityNumber('123456789');
-    expect(instanceOf.state.socialSecurityNumber).toEqual('123456789');
+ // Input Component matches the length 
+it('Input text matches with length', () => {
+  const instanceOf = renderer.create(<VerifySSN />).getInstance();
+  instanceOf.setSocialSecurityNumber('123456789');  
+  expect(instanceOf.state.socialSecurityNumber).toHaveLength(9);
 });
 
+ // Input Component matches with given input
+it('Input text matches with given input',()=>{
+  const instanceOf = renderer.create(<VerifySSN />).getInstance();
+  instanceOf.setSocialSecurityNumber('123456789');
+  expect(instanceOf.state.socialSecurityNumber).toEqual('123456789');
+});
 
+// mocked function call on button click
+it('Called function on button click', () => {       
+  const wrapper = shallow(<VerifySSN onPress={jest.fn()} />)
+  const spy = jest.spyOn(wrapper.instance(), 'onClickNext');
+  wrapper.instance().forceUpdate();
+  wrapper.find(GButtonComponent).at(1).simulate('press');
+  expect(spy).toHaveBeenCalled();
+  spy.mockClear();  
+});
 
+// // mocked function call on button click
+// it('should call mocked function', () => {  
+//   const mockedfn = jest.fn();
+//   const wrapper = shallow(<VerifySSN />);
+
+//   expect(wrapper.find(GButtonComponent).first()).toHaveLength(1);
+//   wrapper.find(GButtonComponent).first().props().onPress();
+//   wrapper.find(GButtonComponent).first().simulate('press');
+//   expect(mockedfn).toHaveBeenCalledTimes(1);  
+  
+// });
 
 });
