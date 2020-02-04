192d2804c8c81985f16f0ecbbad9d174f26a0b5f
Manikandan K <manikandan.k34a613@cognizant.com>
2020-02-04 13:16:50 +0530

    Added account summary title.

--- App/Constants/GlobalStrings.js
+++ App/Constants/GlobalStrings.js
@@ -133,7 +133,8 @@ export default {
         termsAndConditions: "Terms and Conditions",
         termsAndConditionsNote: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
         basicTerms: "Basic Terms",
-        agreeMsgContent: "By checking this box, I acknowledge that I have read the preceding terms and condition and that I understand, assent to, and agree to comply with, those terms and conditions"
+        agreeMsgContent: "By checking this box, I acknowledge that I have read the preceding terms and condition and that I understand, assent to, and agree to comply with, those terms and conditions",
+        accountSummary: "Account Summary",
 
     },
     accManagement: {
