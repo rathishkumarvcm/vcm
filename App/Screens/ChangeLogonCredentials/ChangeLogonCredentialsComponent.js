27e0d5b12ae8962d1777c3b8adacaf777b41ac17
Sudharani <sudharani.gujjulwar@cognizant.com>
2020-02-04 12:55:26 +0530

    Fixed es lint issues for user management- change logon credentials screen

--- App/Screens/ChangeLogonCredentials/ChangeLogonCredentialsComponent.js
+++ App/Screens/ChangeLogonCredentials/ChangeLogonCredentialsComponent.js
@@ -9,39 +9,45 @@ class ChangeLogonCredentialsComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true,
         };
     }
 
     renderMessage = () => {
-        const message = this.props.navigation.getParam('message');
+        const { navigation } = this.props;
+        const message = navigation.getParam('message');
         if (message) {
-            setTimeout(() => {
-                this.setState({
-                    show: false
-                });
-            }, 4000);
-            //  if (this.state.show) {
                 return (
                 <View style={styles.messageFlex}>
                     <Text style={styles.messageText}>{message}</Text>
                 </View>
-);
-           //  }
+                );
         }
         return null;
     }
 
-    navigateCurrentPassword = () => this.props.navigation.navigate('currentPassword');
+    navigateCurrentPassword = () => {
+        const { navigation } = this.props;
+        const { navigate } = navigation;
+        navigate('currentPassword');
+    }
 
-    navigateCurrentPIN = () => this.props.navigation.navigate('currentPIN');
+    navigateCurrentPIN = () => {
+        const { navigation } = this.props;
+        const { navigate } = navigation;
+        navigate('currentPIN');
+    }
 
-    navigateToSecurityPref = () => this.props.navigation.navigate('securityPreference');
+    navigateToSecurityPref = () =>{
+        const { navigation } = this.props;
+        const { navigate } = navigation;
+        navigate('securityPreference');
+    }
 
     render() {
+        const { navigation } = this.props;
         return (
             <View style={styles.container}>
-                <GHeaderComponent navigation={this.props.navigation} />
+                <GHeaderComponent navigation={navigation} />
                 <ScrollView style={styles.mainFlex}>
                     <TouchableOpacity>
                         <GIcon
