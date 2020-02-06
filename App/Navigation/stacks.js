61bfdec5ece35a0e6da3324c1781014af4b953e7
Ragavan, Prabha (Cognizant) <Prabha.Ragavan@cognizant.com>
2020-02-06 12:14:40 +0530

    Navigation es lint - fixed

--- App/Navigation/stacks.js
+++ App/Navigation/stacks.js
@@ -5,43 +5,43 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import ListView from '../Screens/ListView';
 
 // Drawer
-import ProfileSettings from '../Screens/ProfileSettings/';
-import EditPhoneInformation from '../Screens/EditPhoneInformations/';
-import EditOccupationInfo from '../Screens/EditOccupationInformation/';
-import EditMilitaryInfo from '../Screens/EditMilitaryInformation/';
-import EditRelationshipInfo from '../Screens/EditRelationshipInformation/';
+import ProfileSettings from '../Screens/ProfileSettings';
+import EditPhoneInformation from '../Screens/EditPhoneInformations';
+import EditOccupationInfo from '../Screens/EditOccupationInformation';
+import EditMilitaryInfo from '../Screens/EditMilitaryInformation';
+import EditRelationshipInfo from '../Screens/EditRelationshipInformation';
 
 // Drawer User Management Componenet
-import MarketingandPrivacySettings from '../Screens/MarketingandPrivacy/';
-import EditProfileSettings from '../Screens/EditProfileSettings/';
-import EditAddressSettings from '../Screens/EditAddressInformation/';
-import EditAddressAddNew from '../Screens/EditAddressAddNew/';
-import EditAddPhoneNumber from '../Screens/EditAddNewPhoneNumber/';
-import EditEmailInformation from '../Screens/EditEmailInformation/';
-import EditEmailAddNew from '../Screens/EditEmailInfoAddNew/';
-import EditAddFinancialInfo from '../Screens/EditAddFinancialInformation/';
-import EditFamilyMemberInfo from '../Screens/EditFamilyMemberInformation/';
-import EditFamilyDetail from '../Screens/EditFamilyMemberDetails/';
+import MarketingandPrivacySettings from '../Screens/MarketingandPrivacy';
+import EditProfileSettings from '../Screens/EditProfileSettings';
+import EditAddressSettings from '../Screens/EditAddressInformation';
+import EditAddressAddNew from '../Screens/EditAddressAddNew';
+import EditAddPhoneNumber from '../Screens/EditAddNewPhoneNumber';
+import EditEmailInformation from '../Screens/EditEmailInformation';
+import EditEmailAddNew from '../Screens/EditEmailInfoAddNew';
+import EditAddFinancialInfo from '../Screens/EditAddFinancialInformation';
+import EditFamilyMemberInfo from '../Screens/EditFamilyMemberInformation';
+import EditFamilyDetail from '../Screens/EditFamilyMemberDetails';
 
-// Account Manangement 
-import Dashboard from '../Screens/Dashboard/DashboardComponent';
-import TermsAndConditions from '../Screens/TermsAndConditions/TermsAndConditionsComponent';
-import DashboardAccounts from '../Screens/DashboardAccounts/';
-import OpenAccPageOne from '../Screens/OpenAccPageOne/';
-import OpenAccPageTwo from '../Screens/OpenAccPageTwo/';
-import OpenAccPageThree from '../Screens/OpenAccPageThree/';
-import OpenAccPageFour from '../Screens/OpenAccPageFour/';
-import OpenAccPageFive from '../Screens/OpenAccPageFive/';
-import OpenAccPageSix from '../Screens/OpenAccPageSix/';
-import SpecialtyAccPage from '../Screens/SpecialtyAccPage/SpecialtyAccPageComponent';
-import SpecialtyAccSubmit from '../Screens/SpecialtyAccSubmit/';
-import CollegePlanESA from '../Screens/CollegePlanESA/CollegePlanESAComponent';
-import CollegePlanPartOneTwo from '../Screens/CollegePlanPartOneTwo/CollegePlanPartOneTwoComponent';
-import CollegePlanPersonal from '../Screens/CollegePlanPersonal/CollegePlanPersonalComponent';
-import CollegePlanBeneficiary from '../Screens/CollegePlanBeneficiary/CollegePlanBeneficiaryComponent';
+// // Account Manangement 
+// import Dashboard from '../Screens/Dashboard/DashboardComponent';
+// import TermsAndConditions from '../Screens/TermsAndConditions/TermsAndConditionsComponent';
+// import DashboardAccounts from '../Screens/DashboardAccounts';
+// import OpenAccPageOne from '../Screens/OpenAccPageOne';
+// import OpenAccPageTwo from '../Screens/OpenAccPageTwo';
+// import OpenAccPageThree from '../Screens/OpenAccPageThree';
+// import OpenAccPageFour from '../Screens/OpenAccPageFour';
+// import OpenAccPageFive from '../Screens/OpenAccPageFive';
+// import OpenAccPageSix from '../Screens/OpenAccPageSix';
+// import SpecialtyAccPage from '../Screens/SpecialtyAccPage/SpecialtyAccPageComponent';
+// import SpecialtyAccSubmit from '../Screens/SpecialtyAccSubmit';
+// import CollegePlanESA from '../Screens/CollegePlanESA/CollegePlanESAComponent';
+// import CollegePlanPartOneTwo from '../Screens/CollegePlanPartOneTwo/CollegePlanPartOneTwoComponent';
+// import CollegePlanPersonal from '../Screens/CollegePlanPersonal/CollegePlanPersonalComponent';
+// import CollegePlanBeneficiary from '../Screens/CollegePlanBeneficiary/CollegePlanBeneficiaryComponent';
 
-// Others
-import InvestmentPlanInfo from '../Screens/InvestmentPlanInfo';
+// // Others
+// import InvestmentPlanInfo from '../Screens/InvestmentPlanInfo';
 
 // Menu
 import AccountServicesComponent from '../Screens/AccountServices';
@@ -88,107 +88,107 @@ const NotificationTabNavigator = createMaterialTopTabNavigator(
     }
 );
 
-const AccountStack = createStackNavigator({
-    dashboard: {
-        screen: Dashboard,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    termsAndConditions: {
-        screen: TermsAndConditions,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    dashboardAccounts: {
-        screen: DashboardAccounts,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    openAccPageOne: {
-        screen: OpenAccPageOne,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    openAccPageTwo: {
-        screen: OpenAccPageTwo,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    openAccPageThree: {
-        screen: OpenAccPageThree,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    openAccPageFour: {
-        screen: OpenAccPageFour,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    openAccPageFive: {
-        screen: OpenAccPageFive,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    openAccPageSix: {
-        screen: OpenAccPageSix,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    specialtyAccPage: {
-        screen: SpecialtyAccPage,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    specialtyAccSubmit: {
-        screen: SpecialtyAccSubmit,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    collegePlanESA: {
-        screen: CollegePlanESA,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    collegePlanPartOneTwo: {
-        screen: CollegePlanPartOneTwo,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    collegePlanPersonal: {
-        screen: CollegePlanPersonal,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    collegePlanBeneficiary: {
-        screen: CollegePlanBeneficiary,
-        navigationOptions: {
-            header: null,
-        }
-    },
-    investmentPlanInfo: {
-        screen: InvestmentPlanInfo,
-        navigationOptions: {
-            header: null,
-        }
-    },
-});
+// const AccountStack = createStackNavigator({
+//     dashboard: {
+//         screen: Dashboard,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     termsAndConditions: {
+//         screen: TermsAndConditions,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     dashboardAccounts: {
+//         screen: DashboardAccounts,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     openAccPageOne: {
+//         screen: OpenAccPageOne,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     openAccPageTwo: {
+//         screen: OpenAccPageTwo,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     openAccPageThree: {
+//         screen: OpenAccPageThree,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     openAccPageFour: {
+//         screen: OpenAccPageFour,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     openAccPageFive: {
+//         screen: OpenAccPageFive,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     openAccPageSix: {
+//         screen: OpenAccPageSix,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     specialtyAccPage: {
+//         screen: SpecialtyAccPage,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     specialtyAccSubmit: {
+//         screen: SpecialtyAccSubmit,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     collegePlanESA: {
+//         screen: CollegePlanESA,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     collegePlanPartOneTwo: {
+//         screen: CollegePlanPartOneTwo,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     collegePlanPersonal: {
+//         screen: CollegePlanPersonal,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     collegePlanBeneficiary: {
+//         screen: CollegePlanBeneficiary,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+//     investmentPlanInfo: {
+//         screen: InvestmentPlanInfo,
+//         navigationOptions: {
+//             header: null,
+//         }
+//     },
+// });
 
-function customTab(props) {
-    return <Tab tabObj={props.object} routeName={props.route} />;
+function customTab(obj) {
+    return <Tab tabObj={obj.object} routeName={obj.route} />;
 }
 
 //  Bottom Tabs
