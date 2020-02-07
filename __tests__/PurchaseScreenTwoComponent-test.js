3be2bd8b77ab714b7ab374b7c981e12a0bf7c229
829963 <sonal.kiranbagdiya@cognizant.com>
2020-02-07 18:00:24 +0530

    snapshot & rendering testing for Purchase

---
+++ __tests__/PurchaseScreenTwoComponent-test.js
@@ -0,0 +1,27 @@
+
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import PurchaseScreenTwoComponent from '../App/Screens/PurchaseScreenTwo/PurchaseScreenTwoComponent';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const navigation = { navigate: jest.fn() };
+
+describe('Purchase Screen Two - Investment Selection', () => {
+
+  // Snap shot 
+  it('snap shot', () => {
+    const tree = renderer.create(<PurchaseScreenTwoComponent navigation={navigation} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // Application Render
+  test('render', () => {
+    const component = shallow(<PurchaseScreenTwoComponent navigation={navigation} />);
+    component.render();
+  });
+
+});
