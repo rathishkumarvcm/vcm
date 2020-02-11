2f7b9406d1d5d7d57b2069bfb3a6598dc4020c2b
rathikum <47818365+rathishkumarcognizant@users.noreply.github.com>
2020-02-11 12:38:49 +0530

    Update GButtonComponent.js

--- App/CommonComponents/GButtonComponent.js
+++ App/CommonComponents/GButtonComponent.js
@@ -4,42 +4,41 @@ import PropTypes from "prop-types";
 
 import { scaledHeight } from '../Utils/Resolution';
 
-
 /* **************************\
   Function: GButtonComponent
   Explanation:
   Custom component for Button, where ever in project need of
   Buttons this can be resued by giving  props value
+
+  Developer : Rathish
 \************************** */
-const styles = StyleSheet.create({
-buttonStyle:{
-  alignItems:'center',
-  backgroundColor:"#56565A",
-  borderRadius:1,
-  height:scaledHeight(40),
-  justifyContent: "center",
-  marginTop:'2%',
-  width:'90%' 
-  
-},
-buttonTextStyle:{
-  fontSize: scaledHeight(14),
-  fontWeight: "bold",
-  lineHeight:scaledHeight(20),
+const createStyles = (props) => { 
+return StyleSheet.create({
+    buttonStyle:{
+      alignItems:'center',
+      backgroundColor:"#56565A",
+      borderRadius:1,
+      height:scaledHeight(props.height),
+      justifyContent: "center",
+      marginTop:'2%',
+      width:'90%',
+      ...props.buttonStyle
 }
 });
+};
 
 export const GButtonComponent = props => {
-  const {onPress,disabled,buttonStyle,textStyle,buttonText}=props;
+  const {onPress,disabled,textStyle,buttonText}=props;
+  const styles = createStyles(props);
   return(
   <TouchableOpacity 
     onPress={onPress}
     disabled={disabled}
-    style={[styles.buttonStyle,buttonStyle]}
+    style={styles.buttonStyle}
     activeOpacity={0.8}
   >
    {textStyle ? <Text style={textStyle}>{buttonText}</Text> :
-   <Text style={[styles.buttonTextStyle,textStyle]}>{buttonText}</Text>
+   <Text style={styles.buttonStyle}>{buttonText}</Text>
 
    } 
   </TouchableOpacity>
@@ -49,7 +48,7 @@ export const GButtonComponent = props => {
 
 GButtonComponent.propTypes = {
   disabled : PropTypes.bool,
-  buttonStyle: PropTypes.instanceOf(Object),
+  // buttonStyle: PropTypes.func,
   textStyle: PropTypes.instanceOf(Object),
   buttonText: PropTypes.string.isRequired,
   onPress: PropTypes.func
@@ -57,7 +56,7 @@ GButtonComponent.propTypes = {
 
 GButtonComponent.defaultProps = {
   disabled : false,
-  buttonStyle: {},
+  // buttonStyle: () => {},
   textStyle: {},
   onPress: PropTypes.func
 };
